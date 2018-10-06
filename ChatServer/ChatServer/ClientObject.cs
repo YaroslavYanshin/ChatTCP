2b2c421f7fa6cfbe8c71148fd24ac9c91c4eb0e4
YaroslavYanshyn <38620931+YaroslavYanshyn@users.noreply.github.com>
2018-10-06 19:35:17 +0300

    Add gitignore. Creating a new project ChatServer and adding classes ServerObject and ClientObject

---
+++ ChatServer/ChatServer/ClientObject.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+
+namespace ChatServer
+{
+    class ClientObject
+    {
+        protected internal string Id { get; private set; }
+        protected internal NetworkStream Stream { get; private set; }
+        string _userName;
+        TcpClient _client;
+        ServerObject _server;
+
+        public ClientObject(TcpClient tcpClient, ServerObject serverObject)
+        {
+            Id = Guid.NewGuid().ToString();
+            _client = tcpClient;
+            _server = serverObject;
+            serverObject.AddConnection(this);
+        }
+
+        public void Process()
+        {
+            try
+            {
+                Stream = _client.GetStream();
+                string message = GetMessage();
+                _userName = message;
+
+                message = _userName + "вошел в чат";
+                _server.BroadcastMessage(message, this.Id);
+                Console.WriteLine(message);
+
+                while (true)
+                {
+                    try
+                    {
+                        message = GetMessage();
+                        message = String.Format("{0}: {1}", _userName, message);
+                        Console.WriteLine(message);
+                        _server.BroadcastMessage(message, this.Id);
+                    }
+                    catch
+                    {
+                        message = String.Format("{0}: покинул чат", _userName);
+                        Console.WriteLine(message);
+                        _server.BroadcastMessage(message, this.Id);
+                        break;
+                    }
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                // в случае выхода из цикла закрываем ресурсы
+                _server.RemoveConnection(this.Id);
+                Close();
+            }
+            
+
+        }    
+        
+        private string GetMessage()
+        {
+            byte[] data = new byte[64];
+            StringBuilder builder = new StringBuilder();
+            int _bytes = 0;
+            do
+            {
+                _bytes = Stream.Read(data, 0, data.Length);
+                builder.Append(Encoding.Unicode.GetString(data, 0, _bytes));
+            }
+            while (Stream.DataAvailable);
+
+            return builder.ToString();
+        }
+
+        protected internal void Close()
+        {
+            if (Stream != null)
+                Stream.Close();
+            if (_client != null)
+                _client.Close();
+        }
+    }
+}
+        
+    
+
---
+++ ChatServer/ChatServer/ServerObject.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace ChatServer
+{
+    class ServerObject
+    {
+        static TcpListener _tcpListener;
+        List<ClientObject> clients = new List<ClientObject>();
+
+        protected internal void AddConnection (ClientObject clientObject)
+        {
+            clients.Add(clientObject);
+        }
+        protected internal void RemoveConnection (string id)
+        {
+            ClientObject client = clients.FirstOrDefault(c => c.Id == id);
+            if (client != null)
+                clients.Remove(client);
+        }
+
+        protected internal void Listen()
+        {
+            try
+            {
+                _tcpListener = new TcpListener(IPAddress.Any, 8888);
+                _tcpListener.Start();
+                Console.WriteLine("Сервер запущен.Ожидание подключений...");
+
+                while (true)
+                {
+                    TcpClient _tcpClient = _tcpListener.AcceptTcpClient();
+
+                    ClientObject _clientObject = new ClientObject(_tcpClient, this);
+                    Thread _clientThread = new Thread(new ThreadStart(_clientObject.Process));
+                    _clientThread.Start();
+
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Disconnect();
+            }
+        }
+
+        protected internal void BroadcastMessage(string message, string id)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            for (int i = 0; i < clients.Count; i++)
+            {
+                if(clients[i].Id!=id)
+                {
+                    clients[i].Stream.Write(data, 0, data.Length);
+                }
+            }
+        }
+
+        protected internal void Disconnect()
+        {
+            _tcpListener.Stop();
+            for (int i = 0; i < clients.Count; i++)
+            {
+                clients[i].Close();
+            }
+            Environment.Exit(0);
+        }
+    }
+}
