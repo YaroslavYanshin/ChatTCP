d77c1139e98bafe19a484741bebebbc68b671c5b
YaroslavYanshyn <38620931+YaroslavYanshyn@users.noreply.github.com>
2018-10-06 19:44:03 +0300

    changed class Program

--- ChatServer/ChatServer/Program.cs
+++ ChatServer/ChatServer/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace ChatServer
 {
     class Program
     {
+        static ServerObject _server;
+        static Thread _listenThread;
         static void Main(string[] args)
         {
+            try
+            {
+                _server = new ServerObject();
+                _listenThread = new Thread(new ThreadStart(_server.Listen));
+                _listenThread.Start();
+            }
+            catch (Exception ex)
+            {
+                _server.Disconnect();
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
